bf24b7e4ed846e9f8728616765a562feb8b68eda
Justin <jmikey21@gmail.com>
2025-09-02 19:20:17 -0700

    commit

--- Lab4A.cpp
+++ Lab4A.cpp
@@ -1,2 +1,16 @@
 // COMSC-210 | Lab 4A | Justin Mikesell
 // IDE used: Visual Studio Code 
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+struct Color {
+    int red;
+    int green;
+    int blue;
+};
+
+int main() {
+    //declaring vector of Color
+    vector<Color> colors;
